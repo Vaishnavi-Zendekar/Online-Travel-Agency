ef5b086bf12c7e3a87ff2136d95cbabb6a7cd822
Vaishnavi Zendekar <167539175+Vaishnavi-Zendekar@users.noreply.github.com>
2024-10-17 10:44:10 +0530

    Add files via upload

---
+++ Phase_2.js
@@ -0,0 +1,14 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function openForm_1(){
+    document.getElementById("myForm_1").style.display="block";
+  }
+  function closeForm_1(){
+    document.getElementById("myForm_1").style.display="none";
+  }
+  
